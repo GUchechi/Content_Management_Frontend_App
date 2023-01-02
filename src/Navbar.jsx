1e06ac84c5fc4e1c2d6d2a4e1bc605c2759c6712
GUchechi <godswilluchendu7@gmail.com>
2023-01-02 10:14:50 +0100

    added hero section

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
   return (
     <div className="w-screen h-[80px] z-10 bg-zinc-200 fixed drop-shadow-lg">
       <div className="px-2 flex justify-between items-center w-full h-full">
-        <div className="flex items-center">
+        <div className="flex items-center cursor-pointer">
           <h1 className="text-3xl font-bold mr-4 sm:text-4xl">BRAND</h1>
           <ul className="hidden md:flex">
             <li>
