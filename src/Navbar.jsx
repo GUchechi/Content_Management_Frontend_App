50e87274b0523fd389e8a5be91c05c76578a524f
GUchechi <godswilluchendu7@gmail.com>
2023-01-02 02:10:22 +0100

    added navbar

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
     <div className='w-screen h-[80px] z-10 bg-zinc-200 fixed drop-shadow-lg'>
       <div className='px-2 flex justify-between items-center w-full h-full'>
         <div className='flex items-center'>
-          <h1 className='text-3xl font-bold mr-4 sm:text-4xl'>BRAND.</h1>
+          <h1 className='text-3xl font-bold mr-4 sm:text-4xl'>BRAND</h1>
           <ul className='hidden md:flex'>
           <li><Link to="home" smooth={true} duration={500}>Home</Link></li>
           <li><Link to="about" smooth={true} offset={-200} duration={500}>About</Link></li>
@@ -35,7 +35,7 @@ const Navbar = () => {
         </div>
       </div>
 
-      <ul className={!nav ? 'hidden' : 'absolute bg-zinc-200 w-full px-8'}>
+      <ul className={!nav ? 'hidden' : 'absolute bg-zinc-200 w-full px-8 md:hidden'}>
           <li className='border-b-2 border-zinc-300 w-full'><Link onClick={handleClose} to="home" smooth={true} duration={500}>Home</Link></li>
           <li className='border-b-2 border-zinc-300 w-full'><Link onClick={handleClose} to="about" smooth={true} offset={-200} duration={500}>About</Link></li>
           <li className='border-b-2 border-zinc-300 w-full'><Link onClick={handleClose} to="support" smooth={true} offset={-50} duration={500}>Support</Link></li>
